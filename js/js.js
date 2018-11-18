f64db3839cc0530823bc061355082639314bcdec
Oleg Romanovsky <mad.cat6991@gmail.com>
2018-11-18 03:48:50 +0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,36 @@
+$('.sl').slick({
+    autoplay: true,
+    autoplaySpeed: 2500,
+    speed: 1000,
+    dots: true,
+    arrows: false,
+    fade: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1,
+            }
+        }
+    ]
+});
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
